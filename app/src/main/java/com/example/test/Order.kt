72e6eade891de1db8b0eb89d5fa4b551ceea7a38
unknown <bettydobre@gmail.com>
2020-05-25 14:52:40 +0300

    added Singleton design pattern and update Place order

--- app/src/main/java/com/example/test/Order.kt
+++ app/src/main/java/com/example/test/Order.kt
@@ -1,5 +1,39 @@
 package com.example.test
 
-class Order (val id:String, val fullname: String, val address:String, val phonenumber:String, val payment:String) {
-    constructor() : this("", "","","","")
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+object Order:Serializable {
+
+    @SerializedName("id")
+    var  id:String
+    @SerializedName("fullname")
+    var  fullname:String
+    @SerializedName("address")
+    var  address:String
+    @SerializedName("phonenumber")
+    var  phonenumber:String
+    @SerializedName("payment")
+    var  payment:String
+    @SerializedName("message")
+    var  message:String
+    @SerializedName("date")
+    var date:String
+    @SerializedName("price")
+    var price:String
+    @SerializedName("command")
+    var  command:String
+
+    init {
+        id = ""
+        fullname = ""
+        address = ""
+        phonenumber = ""
+        payment = ""
+        message = ""
+        date = ""
+        price = ""
+        command = ""
+    }
+
 }
